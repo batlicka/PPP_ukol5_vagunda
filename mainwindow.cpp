4ec05e43bf496c5c16b4c5591140c4ec10132291
batlicka <46314552+batlicka@users.noreply.github.com>
2019-05-15 15:25:06 +0200

    odstraneny zavorky

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,12 +27,12 @@ void MainWindow::on_ButtonCalculatePI_clicked()
     double result=0;
     int NProc = NumberOfProcessors;
 
-#pragma omp parallel num_threads(NProc) private(N) shared(result){
+#pragma omp parallel num_threads(NProc) private(N) shared(result)
         #pragma omp for reduction (+:result)
         for(int i=0; i<N;i++){
             result+=(1/N)*(4/(1+ ((i+0.5)/N)*((i+0.5)/N) ));
         }
-    }
+    
     ui->lineEditResult->setText(QString::number(result));
 
 }
